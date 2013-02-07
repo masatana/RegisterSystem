af64774a3c7f6b94011b3feb62c492eb72190ed7
masatana <plaza.tumbling@gmail.com>
2013-02-08 00:05:56 +0900

    Change little bit

--- createdb.sql
+++ createdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE member_list (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    no INT NOT NULL,
+    no INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL,
     name_uji VARCHAR(30) NOT NULL,
     name_na VARCHAR(30) NOT NULL,
     name_uji_yomi VARCHAR(50) NOT NULL,
@@ -9,6 +9,7 @@ CREATE TABLE member_list (
     birth DATE NOT NULL,
     age INT NOT NULL,
     blood_type VARCHAR(2) NOT NULL,
-    course TEXT NOT NULL,
+    course_name TEXT NOT NULL,
+    course_charge INT NOT NULL,
     special_report TEXT
 );
--- regist.php
+++ regist.php
@@ -4,7 +4,15 @@
 <?php
 session_start();
 require_once "definition.php";
-echo $_SESSION[NAME_UJI];
+$s = mysql_connect("localhost", "root") or die("failed");
+echo "Suceess";
+mysql_select_db("test");
+
+$table = mysql_query("INSERT INTO test (id, name_uji, name_na, name_uji_yomi, name_na_yomi, mail_address,
+    birth, age, blood_type, course_name, course_charge, special_report) VALUES ('10', '$_SESSION[NAME_UJI]',
+    '$_SESSION[NAME_NA]', '$_SESSION[NAME_UJI_YOMI]', '$_SESSION[NAME_NA_YOMI]', $_SESSION[MAIL_ADDRESS]',
+    '1990/10/04', '22', '$_SESSION[BLOOD_TYPE]', '$_SESSION[COURSE_NAME]', '3000', '$_SESSION[SPECIAL_REPORT]'");
+mysql_close($s);
 ?>
 </body>
 </html>
